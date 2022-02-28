9353d31fc32c39b4746172969bb19610f9c2fdbc
Orange Joe <99784115+Orange-Joe@users.noreply.github.com>
2022-02-27 21:10:21 -0600

    Create pass_gatherer.py

---
+++ pass_gatherer.py
@@ -0,0 +1,22 @@
+import subprocess
+
+
+''' Quickly print out cleartext WiFi passwords stored on a Windows 10 machine '''
+__author__ = "Orange-Joe"
+__copyright__ = "Orange-Joe"
+__version__ =  "1.0"
+
+
+# Windows command prints out information on WiFi profiles. Output is stripped of unneccessary characters and put into a list. 
+output = subprocess.getoutput("""for /f "skip=9 tokens=1,2 delims=:" %i in ('netsh wlan show profiles') do @echo %j | findstr -i -v echo | netsh wlan show profiles %j key=clear""")
+output = output.replace(' ', '')
+output = output.split("\n")
+
+
+# Print out useful information. 
+for i in output:
+    if i[0:8] == "SSIDname":
+        print(i)
+    if i[0:10] == "KeyContent":
+        print(i)
+        print("\n")
